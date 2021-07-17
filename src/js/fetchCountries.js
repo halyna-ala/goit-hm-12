8b73fd5ab87a2eb42a19894875e7c02854f1fb1f
halyna-ala <77680693+halyna-ala@users.noreply.github.com>
2021-07-18 00:12:58 +0200

    import

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,8 @@
+import markupOfSingleCountry from ''
+import markupOfFewCountries from ''
+import Notiflix from "notiflix";
+
+
 const BASIC_URL = 'https://restcountries.eu/rest/v2/name/';
 const OPTIONS = 'fields=name;capital;population;flag;languages'
 const markup = document.querySelector('.country-list')
